f9bdc2c039999461f6809520f7640bb8a12a239c
Joehoel <31251240+Joehoel@users.noreply.github.com>
2020-07-27 11:26:51 +0200

    Made better poll

--- src/commands/poll.js
+++ src/commands/poll.js
@@ -1,25 +1,130 @@
 const Discord = require("discord.js")
 
+const options = [
+	"🇦",
+	"🇧",
+	"🇨",
+	"🇩",
+	"🇪",
+	"🇫",
+	"🇬",
+	"🇭",
+	"🇮",
+	"🇯",
+	"🇰",
+	"🇱",
+	"🇲",
+	"🇳",
+	"🇴",
+	"🇵",
+	"🇶",
+	"🇷",
+	"🇸",
+	"🇹",
+	"🇺",
+	"🇻",
+	"🇼",
+	"🇽",
+	"🇾",
+	"🇿",
+]
+
+const pollLog = {}
+
+function canSendPoll(user_id) {
+	if (pollLog[user_id]) {
+		const timeSince = Date.now() - pollLog[user_id].lastPoll
+		if (timeSince < 30000) {
+			return false
+		}
+	}
+	return true
+}
+
+function isAdmin(member) {
+	if (!member.hasPermission("ADMINISTRATOR")) return false
+	return true
+}
+
 module.exports = {
 	name: "poll",
-	description: "generates a poll from input",
-	usage: "!poll <question>",
+	triggers: ["poll", "📊"],
+	description:
+		"Ask a polling question. Vote by emoji reaction. Question and options must be wrapped in double quotes. Questions with no provided options are treated as Yes / No / Unsure questions.",
+	usage: "<question> <optional answer A> <optional answer B>",
 	args: true,
-	execute({ channel, author }, args) {
-		const question = args.slice(0).join(" ")
-
-		const poll = new Discord.MessageEmbed()
-			.setColor("#ffc600")
-			.setTitle(question)
-			.setFooter(
-				`Poll started by: ${author.username}`,
-				`${author.avatarURL()}`
+	execute(message) {
+		let args = message.content.match(/"(.+?)"/g)
+		if (args) {
+			if (!canSendPoll(message.author.id) && !isAdmin(message.member)) {
+				return message.channel.send(
+					`${message.author} please wait before sending another poll.`
+				)
+			} else if (args.length === 1) {
+				// yes no unsure question
+				const question = args[0].replace(/"/g, "")
+				pollLog[message.author.id] = {
+					lastPoll: Date.now(),
+				}
+				return message.channel
+					.send(
+						new Discord.MessageEmbed()
+							.setColor("#ffc600")
+							.setTitle(question)
+							.setFooter(
+								`Poll started by: ${message.author.username}`,
+								`${message.author.avatarURL()}`
+							)
+							.setTimestamp()
+					)
+					.then(async pollMessage => {
+						await pollMessage.react("👍")
+						await pollMessage.react("👎")
+						await pollMessage.react("🤷‍♀️")
+					})
+			} else {
+				// multiple choice
+				args = args.map(a => a.replace(/"/g, ""))
+				const question = args[0]
+				const questionOptions = [...new Set(args.slice(1))]
+				if (questionOptions.length > 20) {
+					return message.channel.send(
+						`${message.author} Polls are limited to 20 options.`
+					)
+				} else {
+					pollLog[message.author.id] = {
+						lastPoll: Date.now(),
+					}
+					return message.channel
+						.send(
+							new Discord.MessageEmbed()
+								.setColor("#ffc600")
+								.setTitle(question)
+								.setDescription(
+									`${questionOptions
+										.map(
+											(option, i) =>
+												`${options[i]} - ${option}`
+										)
+										.join("\n")}`
+								)
+								.setFooter(
+									`Poll started by: ${message.author.username}`,
+									`${message.author.avatarURL()}`
+								)
+								.setTimestamp()
+						)
+						.then(async pollMessage => {
+							for (let i = 0; i < questionOptions.length; i++) {
+								await pollMessage.react(options[i])
+							}
+						})
+				}
+			}
+		} else {
+			return message.channel.send(
+				`${message.author} invalid Poll! Question and options should be wrapped in double quotes.`
 			)
-			.setTimestamp()
-		channel.send(poll).then(message => {
-			message.react("👍")
-			message.react("👎")
-			message.react("🤷‍♀️")
-		})
+		}
 	},
 }
