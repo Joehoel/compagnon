7612358bdf620871dd54c25cf6c8edaff3e51ca6
Joehoel <31251240+Joehoel@users.noreply.github.com>
2020-07-29 11:12:08 +0200

    Improved ping command

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -4,7 +4,19 @@ module.exports = {
 	name: "ping",
 	description: "Ping!",
 	execute(message) {
-		const embed = new Discord.MessageEmbed().setTitle("Pong")
-		message.channel.send(embed)
+		const embed = new Discord.MessageEmbed({
+			title: "Pong",
+			color: "ffc600",
+		})
+		message.channel.send(embed).then(m => {
+			const ping = m.createdTimestamp - message.createdTimestamp
+			m.edit(
+				new Discord.MessageEmbed({
+					title: "Pong",
+					color: "ffc600",
+					description: `${ping}ms`,
+				})
+			)
+		})
 	},
 }
