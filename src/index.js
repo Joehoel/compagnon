19c52199ca9b4bae8b22b4079eb6c0c6c9b460d3
Joehoel <31251240+Joehoel@users.noreply.github.com>
2020-09-13 13:58:12 +0200

    Changed some shit

--- src/index.js
+++ src/index.js
@@ -1,59 +1,59 @@
-const fs = require("fs")
-require("dotenv").config()
-const { TOKEN, PREFIX } = process.env
+const fs = require("fs");
+require("dotenv").config();
+const { TOKEN, PREFIX } = process.env;
 
-const Discord = require("discord.js")
-const client = new Discord.Client()
+const Discord = require("discord.js");
+const client = new Discord.Client();
 
-client.commands = new Discord.Collection()
+client.commands = new Discord.Collection();
 
 const commandFiles = fs
 	.readdirSync("./src/commands")
-	.filter(file => file.endsWith(".js"))
+	.filter(file => file.endsWith(".js"));
 
 for (const file of commandFiles) {
-	const command = require(`./commands/${file}`)
+	const command = require(`./commands/${file}`);
 
-	client.commands.set(command.name, command)
+	client.commands.set(command.name, command);
 }
 
 client.once("ready", () => {
-	console.log("Compagnon online!")
-})
+	console.log("Compagnon online!");
+});
 
 client.on("message", message => {
-	if (!message.content.startsWith(PREFIX) || message.author.bot) return
+	if (!message.content.startsWith(PREFIX) || message.author.bot) return;
 
-	const args = message.content.slice(PREFIX.length).trim().split(/ +/)
+	const args = message.content.slice(PREFIX.length).trim().split(/ +/);
 
-	const commandName = args.shift().toLowerCase()
+	const commandName = args.shift().toLowerCase();
 
-	if (!client.commands.has(commandName)) return
+	if (!client.commands.has(commandName)) return;
 
-	const command = client.commands.get(commandName)
+	const command = client.commands.get(commandName);
 
 	if (command.admin && !message.member.hasPermission("ADMINISTRATOR")) {
 		return message.channel.send(
 			`Sorry, ${message.author}! You must be an admin to execute this command.`
-		)
+		);
 	}
 
 	if (command.args && !args.length) {
-		let reply = `You didn't provide any arguments, ${message.author}!`
+		let reply = `You didn't provide any arguments, ${message.author}!`;
 
 		if (command.usage) {
-			reply += `\nThe proper usage would be: \`${PREFIX}${command.name} ${command.usage}\``
+			reply += `\nThe proper usage would be: \`${PREFIX}${command.name} ${command.usage}\``;
 		}
 
-		return message.channel.send(reply)
+		return message.channel.send(reply);
 	}
 
 	try {
-		command.execute(client, message, args)
+		command.execute(client, message, args);
 	} catch (error) {
-		console.error(error)
-		message.reply("there was an error trying to execute that command!")
+		console.error(error);
+		message.reply("there was an error trying to execute that command!");
 	}
-})
+});
 
-client.login(TOKEN)
+client.login(TOKEN);
