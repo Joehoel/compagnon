0bc6ae70883ad20e54eaf026ae5a04083de262ee
Joehoel <31251240+Joehoel@users.noreply.github.com>
2020-11-11 13:53:13 +0100

    fix

--- src/commands/dab.js
+++ src/commands/dab.js
@@ -1,8 +1,6 @@
 module.exports = {
 	name: "dab",
 	description: "Shows dab GIF",
-	args: true,
-	admin: false,
 	execute(client, message) {
 		return message.channel.send("https://media.giphy.com/media/bXvwCQglnTGKs/giphy.gif")
 	},
