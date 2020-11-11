cd5013985289c53268468269009788ac6bdc6dee
Joehoel <31251240+Joehoel@users.noreply.github.com>
2020-11-11 13:58:04 +0100

    Added kick.js & ban.js

---
+++ src/commands/ban.js
@@ -0,0 +1,10 @@
+module.exports = {
+	name: "Ban",
+	description: "Ban someone",
+	args: true,
+	admin: true,
+	usage: "<username> <reason>",
+	execute(client, message, args) {
+		// TODO: Add ban command
+	},
+};
---
+++ src/commands/kick.js
@@ -0,0 +1,10 @@
+module.exports = {
+	name: "Kick",
+	description: "Kick someone",
+	args: true,
+	admin: true,
+	usage: "<username> <reason>",
+	execute(client, message, args) {
+		// TODO: Add kick command
+	},
+};
