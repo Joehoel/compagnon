aacc9e16cc9ef366851434a53603b3c3d75bed88
Joehoel <31251240+Joehoel@users.noreply.github.com>
2021-01-20 19:56:13 +0100

    Add comments

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,28 @@
 // Global
 import "dotenv/config";
 import "module-alias/register";
-import music from "@/features/music";
-import Command from "@/utils/Command";
-import colors from "colors";
-import { Client, Collection } from "discord.js";
-import DisTube from "distube";
-import Commands from "./commands";
-import command from "./features/command";
-import { createConnection } from "typeorm";
-const { TOKEN } = process.env;
-import consola from "consola";
 
+// Handlers
+import music from "@/features/music";
+import command from "./features/command";
+
+// Command related
+import Command from "@/utils/Command";
+import Commands from "./commands";
+
+// Other
+import colors from "colors";
+import consola from "consola";
+import DisTube from "distube";
+import { Client, Collection } from "discord.js";
+import { createConnection } from "typeorm";
+
+// Environment variables
+const { TOKEN } = process.env;
+
+// Register new discord client
 const client = new Client();
 
-// Command handler
 client.commands = new Collection<string, Command>();
 client.aliases = new Collection<string, string>();
 client.music = new DisTube(client, { searchSongs: false, emitNewSongOnly: true });
@@ -23,8 +31,10 @@ client.logger = consola;
 for (const file in Commands) {
     const command = Commands[file as keyof typeof Commands];
 
+    // Register command
     client.commands.set(file, command);
 
+    // Register all command aliases
     command.aliases.forEach((alias: string) => {
         client.aliases.set(alias, file);
     });
@@ -33,13 +43,13 @@ for (const file in Commands) {
 // Ready!
 client.on("ready", async () => {
     try {
-        await createConnection();
-        client.logger.success("Database" + colors.green.bold(" connected!"));
-
         // Music handler
         music(client.music);
-
         client.logger.success("Compagnon" + colors.green.bold(" online!"));
+
+        // Database connection
+        await createConnection();
+        client.logger.success("Database" + colors.green.bold(" connected!"));
     } catch (error) {
         client.logger.error(error);
     }
@@ -51,4 +61,5 @@ client.on("message", async (message) => {
     command(client, message);
 });
 
+// Login
 client.login(TOKEN);
