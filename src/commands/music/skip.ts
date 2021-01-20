02899c943ef615cba7b7ab2f52c88ff49532a82c
Joehoel <31251240+Joehoel@users.noreply.github.com>
2021-01-20 11:33:47 +0100

    Update skip command

--- src/commands/music/skip.ts
+++ src/commands/music/skip.ts
@@ -4,8 +4,12 @@ import Command from "@/utils/Command";
 export default new Command({
     name: "skip",
     description: "Skip a song",
+    aliases: ["jump"],
+    usage: "<song number>",
     roles: ["521378616429248527"],
-    execute(client, message) {
-        return client.music.skip(message);
+    execute(client, message, args) {
+        const songNumber = parseInt(args[0]);
+        if (!songNumber) return client.music.skip(message);
+        return client.music.jump(message, songNumber);
     },
 });
