3b41844e817d1c85901df485e6de65ab4cc7adf1
Joehoel <31251240+Joehoel@users.noreply.github.com>
2021-01-22 16:47:17 +0100

    Things

--- ormconfig.js
+++ ormconfig.js
@@ -2,8 +2,7 @@ const rootDir = process.env.NODE_ENV === "development" ? "src" : "dist";
 
 module.exports = {
     type: "postgres",
-    url:
-        "postgres://cilvstuuugjzya:0baf54bc161a9d61310bde388ec68d193ec03466d61dcf0316b8bd9beb6a0b55@ec2-54-247-78-30.eu-west-1.compute.amazonaws.com:5432/d7d6oiilaief2d",
+    url: process.env.DATABASE_URL,
     synchronize: true,
     logging: false,
     ssl: true,
