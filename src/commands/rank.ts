0240b14f0f816e13896998715f2edadd3447301c
Joehoel <31251240+Joehoel@users.noreply.github.com>
2021-04-20 11:22:04 +0200

    Fix typo

--- src/commands/rank.ts
+++ src/commands/rank.ts
@@ -21,7 +21,7 @@ export default new Command({
           title: "Rank",
           description: `**${target.username}** is currently level \`${user.level}\` \`(${user.xp}/${Levels.xpFor(
             user.level + 1
-          )}\`)`,
+          )})\``,
         },
         message
       )
