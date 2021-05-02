83153a1718ef96e0d2740477de73ea36330333e7
Joehoel <31251240+Joehoel@users.noreply.github.com>
2021-05-02 17:19:44 +0200

    Move command reader to ready event

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -2,10 +2,25 @@ import colors from "colors";
 import { Client } from "discord.js";
 import { createConnection } from "typeorm";
 import { music } from "../features";
+import Command from "../utils/Command";
 import { GUILD_ID } from "../utils/constants";
+import { read } from "../utils/read";
 
 export default async (client: Client) => {
   try {
+    // Register all commands
+    const commands = await read<Command>("../commands");
+    for (const command of commands) {
+      // Register command
+      client.commands.set(command.name, command);
+
+      // Register all command aliases
+      if (!command.aliases) return;
+      command.aliases.forEach((alias: string) => {
+        client.aliases.set(alias, command.name);
+      });
+    }
+
     // Music handler
     music(client.music);
     client.logger.success("Compagnon" + colors.green.bold(" online!"));
