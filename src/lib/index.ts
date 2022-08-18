563d2905e0ca83e1b58a9d1cde332519ff6618a9
Joehoel <jmkuijper@outlook.com>
2022-08-18 12:47:52 +0200

    feat: started on leveling

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,7 @@
 export { default as Bot } from "./Bot";
 export { default as Command } from "./Command";
 export { default as Module } from "./Module";
+export * from "./db";
 export * from "./helpers";
+export * from "./lyrics";
 export * from "./read";
--- src/modules/levels.ts
+++ src/modules/levels.ts
@@ -1,16 +1,50 @@
 import { Module } from "@/lib";
+import { db } from "@/lib/db";
+import { differenceInMinutes } from "date-fns";
 
 const { PREFIX } = process.env;
 
+const messages = new Map<string, number[]>();
+const voice = new Map<string, [Date, Date | undefined]>();
+const timer = new Map<string, string>();
+
 export default new Module({
   name: "levels",
-  event: "messageCreate",
-  async run(client, message) {
-    // TODO: implement
+  event: "ready",
+  async run(client) {
+    const minute = new Date().getMinutes();
 
-    if (message.channel.type == "DM") return;
-    if (message.content.startsWith(PREFIX) || message.author.bot) return;
+    client.on("messageCreate", async (message) => {
+      if (message.channel.type == "DM") return;
+      if (message.content.startsWith(PREFIX) || message.author.bot) return;
 
-    const text = message.content.toLowerCase();
+      const levels = await db.level.findFirst({ where: { discordId: message.author.id } });
+
+      if (!levels) {
+        await db.level.create({ data: { discordId: message.author.id } });
+        return;
+      }
+
+      // Check if the user has sent a message in the current minute
+      const sent = messages.get(message.author.id) ?? [];
+
+      if (!sent.includes(minute)) {
+        messages.set(message.author.id, [...sent, minute]);
+
+        // TODO: Make reusable
+        const amount = 100 + levels.level * 100;
+        const level = Math.floor(0.1 * Math.sqrt(amount));
+
+        // Add xp to the user
+        await db.level.update({
+          where: { discordId: message.author.id },
+          data: { xp: levels.xp + amount, level },
+        });
+      }
+    });
+
+    client.on("voiceStateUpdate", async (oldState, newState) => {
+      // TODO: implement
+    }
   },
 });
