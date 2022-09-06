f2ade51380704f837296b3c2a5188bb0dedb4523
Joël Kuijper <joel@deindruk.nl>
2022-09-06 18:13:36 +0200

    feat: add tiny-invariant env variable validation

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,14 @@ import { Intents } from "discord.js";
 import { Bot } from "./lib";
 import "dotenv/config";
 import player from "./lib/music-player";
+import invariant from "tiny-invariant";
 
-const { TOKEN, GUILD_ID, CLIENT_ID } = process.env;
+const { TOKEN, GUILD_ID, CLIENT_ID, DATABASE_URL } = process.env;
+
+invariant(TOKEN, "'TOKEN' environment variable is required");
+invariant(GUILD_ID, "'GUILD_ID' environment variable is required");
+invariant(CLIENT_ID, "'CLIENT_ID' environment variable is required");
+invariant(DATABASE_URL, "'DATABASE_URL' environment variable is required");
 
 export const bot = new Bot({
   token: TOKEN,
