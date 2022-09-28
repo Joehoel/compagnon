904d6e857bd6804ff0e066e102ea187429bc5ac8
joelkuijper <jmkuijper@outlook.com>
2022-09-28 14:51:19 +0200

    feat: add uwu command

---
+++ src/commands/uwu.ts
@@ -0,0 +1,21 @@
+import { ApplicationCommandOptionType } from "discord-api-types/v9";
+import Uwuifier from "uwuifier";
+import Command from "../lib/Command";
+
+const uwu = new Uwuifier();
+
+export default new Command({
+  name: "uwu",
+  description: "uwuify a sentence",
+  options: [
+    {
+      description: "the sentence to uwuify",
+      type: ApplicationCommandOptionType.String,
+      required: true,
+      name: "sentence",
+    },
+  ],
+  execute(_, interaction) {
+    return interaction.reply(uwu.uwuifySentence(interaction.options.getString("sentence")!));
+  },
+});
