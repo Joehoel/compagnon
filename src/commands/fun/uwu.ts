7447da14460d37d1045e8778dda6806639ca556f
joelkuijper <jmkuijper@outlook.com>
2022-09-28 15:05:13 +0200

    feat: remove ephemeral from mock and uwu

--- src/commands/fun/uwu.ts
+++ src/commands/fun/uwu.ts
@@ -1,6 +1,6 @@
 import { ApplicationCommandOptionType } from "discord-api-types/v9";
 import Uwuifier from "uwuifier";
-import Command from "../lib/Command";
+import Command from "../../lib/Command";
 
 const uwu = new Uwuifier();
 
@@ -18,7 +18,6 @@ export default new Command({
   execute(_, interaction) {
     return interaction.reply({
       content: uwu.uwuifySentence(interaction.options.getString("sentence")!),
-      ephemeral: true,
     });
   },
 });
